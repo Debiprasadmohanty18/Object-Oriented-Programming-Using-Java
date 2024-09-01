024f317984fce24e49f88717611b07356c95c78b
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-01 13:03:07 +0530

    Add files via upload

---
+++ packages/PackageConcept.java
@@ -0,0 +1,14 @@
+package com.packages;
+
+public class PackageConcept 
+{
+	public int jerseyno;
+	public String name;
+	public PackageConcept() 
+	{
+		this.name = name;
+		this.jerseyno = jerseyno;
+	}
+	
+
+}
---
+++ packages/first/AccessPackage.java
@@ -0,0 +1,15 @@
+package com.packages.first;
+
+public class AccessPackage {
+
+	public static void main(String[] args) 
+	{
+		
+	}
+	
+	public void typeSomething()
+	{
+		System.out.println("Hii,");
+	}
+
+}
