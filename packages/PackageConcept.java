e0a991d201df09550768ecbbcd9f71099d4c24e7
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-01 21:40:53 +0530

    Add files via upload

---
+++ packages/PackageConcept.java
@@ -0,0 +1,13 @@
+package com.packages;
+
+public class PackageConcept 
+{
+	public int jerseyno;
+	public String name;
+	public PackageConcept() 
+	{
+		System.out.println("Default Constructor");
+	}
+	
+
+}
