436b03f43b0bc3ba70134eb1752822d3e418da43
Debiprasad Mohanty <145470177+Debiprasadmohanty18@users.noreply.github.com>
2024-09-03 22:54:43 +0530

    Update DefaultModifier.java

--- accessModifiers/Default/DefaultModifier.java
+++ accessModifiers/Default/DefaultModifier.java
@@ -1,4 +1,4 @@
-
+//If you don't use any modifier, it is treated as default by default. The default modifier is accessible only within package. It cannot be accessed from outside the package. It provides more accessibility than private. But, it is more restrictive than protected, and public
 
 package com.accessModifiers.Default;
 
